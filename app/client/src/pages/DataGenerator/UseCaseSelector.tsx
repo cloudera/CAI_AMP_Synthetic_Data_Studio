d2fa5f41b78cac9a6c8a98f00677174728b994b4
Keivan Vosoughi <kvosoughi@cloudera.com>
2025-06-18 16:34:51 -0700

    DSE-45299: Fetching Templates from SDS API for SDS UI
    Add UseCaseSlector

---
+++ app/client/src/pages/DataGenerator/UseCaseSelector.tsx
@@ -0,0 +1,54 @@
+import { Form, Select } from "antd";
+import { FunctionComponent, useEffect, useState } from "react";
+import { useGetUseCases } from "./hooks";
+import { UseCase } from "../../types";
+import get from "lodash/get";
+
+interface Props {}
+
+
+const UseCaseSelector: FunctionComponent<Props> = () => {
+  const [useCases, setUseCases] = useState<UseCase[]>([]);
+  const useCasesReq = useGetUseCases();  
+  console.log('useCasesReq', useCasesReq);
+
+  useEffect(() => {
+    if (useCasesReq.data) {
+        console.log('useCasesReq.data', useCasesReq.data);
+        let _useCases = get(useCasesReq, 'data.usecases', []);
+        _useCases = _useCases.map((useCase: any) => ({ 
+            ...useCase,
+            label: useCase.name, 
+            value: useCase.id 
+        }));
+        console.log('_useCases', _useCases);
+        setUseCases(_useCases);
+    }
+  }, [useCasesReq.data]);
+
+
+  return (
+    <Form.Item
+        name='use_case'
+        label='Template'
+        rules={[
+            { required: true }
+        ]}
+        tooltip='A specialize template for generating your dataset'
+        labelCol={{
+            span: 8
+        }}
+        shouldUpdate
+        >
+            <Select placeholder={'Select a template'}>
+                {useCases.map(option => 
+                    <Select.Option key={option.value} value={option.value}>
+                        {option.label}
+                    </Select.Option>
+                )}
+            </Select>
+    </Form.Item>
+  );
+}
+
+export default UseCaseSelector;
