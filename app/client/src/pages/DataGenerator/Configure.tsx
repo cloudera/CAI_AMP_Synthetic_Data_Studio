1534f9d9a7d5b56c9a9857ec8d229434be677943
Keivan Vosoughi <kvosoughi@cloudera.com>
2025-07-08 19:55:27 -0700

    DSE-45114: Add Data Augmentation Card
    This PR contains following changes:
     - Adds new card for Data Augmentation to the Home Page
     - Hide Workflow Type for the Data Augmentation

--- app/client/src/pages/DataGenerator/Configure.tsx
+++ app/client/src/pages/DataGenerator/Configure.tsx
@@ -8,9 +8,12 @@ import { File, WorkflowType } from './types';
 import { useFetchModels } from '../../api/api';
 import { MODEL_PROVIDER_LABELS } from './constants';
 import { ModelProviders, ModelProvidersDropdownOpts } from './types';
-import { useWizardCtx } from './utils';
+import { getWizardModel, getWizardModeType, useWizardCtx } from './utils';
 import FileSelectorButton from './FileSelectorButton';
 import UseCaseSelector from './UseCaseSelector';
+import { useLocation } from 'react-router-dom';
+import { WizardModeType } from '../../types';
+import { get } from 'lodash';
 
 
 const StepContainer = styled(Flex)`
@@ -39,7 +42,7 @@ export const USECASE_OPTIONS = [
 export const WORKFLOW_OPTIONS = [
     { label: 'Supervised Fine-Tuning', value: 'supervised-fine-tuning' },
     { label: 'Custom Data Generation', value: 'custom' },
-    { label: 'Freeform Data Generation', value: 'freeform' }
+    // { label: 'Freeform Data Generation', value: 'freeform' }
 ];
 
 export const MODEL_TYPE_OPTIONS: ModelProvidersDropdownOpts = [
@@ -48,6 +51,18 @@ export const MODEL_TYPE_OPTIONS: ModelProvidersDropdownOpts = [
 ];
 
 const Configure = () => {
+    const location = useLocation();
+    const [wizardModeType, setWizardModeType] = useState(getWizardModeType(location));
+
+    useEffect(() => {
+        if (wizardModeType === WizardModeType.DATA_AUGMENTATION) {
+            setWizardModeType(WizardModeType.DATA_AUGMENTATION);
+            form.setFieldValue('workflow_type', 'freeform');
+        } else {
+            setWizardModeType(WizardModeType.DATA_GENERATION);
+        }
+    }, [location, wizardModeType]);
+
     const form = Form.useFormInstance();
     const formData = Form.useWatch((values) => values, form);
     const { setIsStepValid } = useWizardCtx();
@@ -141,8 +156,10 @@ const Configure = () => {
                     label='Model Provider'
                     rules={[{ required: true }]}
                     labelCol={labelCol}
+                    shouldUpdate
                 >
                     <Select
+                       
                         onChange={() => form.setFieldValue('model_id', undefined)}
                         placeholder={'Select a model provider'}
                     >
@@ -210,6 +227,7 @@ const Configure = () => {
                     label='Workflow'
                     tooltip='A specialized workflow for your dataset'
                     labelCol={labelCol}
+                    hidden={wizardModeType === WizardModeType.DATA_AUGMENTATION}
                     shouldUpdate
                     rules={[
                             { required: true }
